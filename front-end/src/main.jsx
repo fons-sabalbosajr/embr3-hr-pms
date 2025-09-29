e7345bff230894f13e4c0d90cf46796f943343b6
MIS Server <86940332+fons-sabalbosajr@users.noreply.github.com>
2025-09-29 11:40:22 +0800

    commit 09/29/2025

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,12 +1,15 @@
 // client/src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom"; // ✅ missing import
 import App from './App.jsx';
 import 'antd/dist/reset.css';
-import './index.css';        
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/hrpms">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
